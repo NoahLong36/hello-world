c175bd444482b880af3bb3afe9441c1845e68b53
nel36 <nel36@pitt.edu>
2023-10-08 20:48:59 -0400

    noah commit

---
+++ .ipynb_checkpoints/Untitled-2-checkpoint.py
@@ -0,0 +1,15 @@
+def terminate():
+    status = True
+    run = False
+run = True
+while run:
+    name = input("Name:")
+    sex = input("Sex:")
+    age = input("Age:")
+    if name == "Sarah Connor":
+        if age == "19":
+            if sex == "female":
+                  terminate()
+                  print(status)
+            elif sex == "Female":
+                  terminate()
